8a2bd3e3e99d1437e31ad6b6e5fc7746fe069dca
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-05-14 09:43:16 -0300

    Navbar

--- Front/buenSaborFront/src/assets/componentes/Navegador.tsx
+++ Front/buenSaborFront/src/assets/componentes/Navegador.tsx
@@ -6,12 +6,12 @@ export function Navegador() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Buen Sabor</Navbar.Brand>
+        <Navbar.Brand href="/">Buen Sabor</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/grillaManufacturado">Ariticulos Manufacturados</Nav.Link>
-            <Nav.Link href="/grillaInsumo">Articulos Insumo</Nav.Link>
+            <Nav.Link href={"/grillaInsumo"}>Articulos Insumo</Nav.Link>
+            <Nav.Link href={"/grillaManufacturado"}>Ariticulos Manufacturados</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- Front/buenSaborFront/src/main.tsx
+++ Front/buenSaborFront/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,18 +6,17 @@ import { GrillaArticuloManufacturado } from './assets/componentes/GrillaArticulo
 import { GrillaArticuloInsumo } from './assets/componentes/GrillaArticuloInsumo.tsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navegador } from './assets/componentes/Navegador.tsx'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Navegador />
       <Routes>
-        <Route path="/app" element={<App />} />
-        {/* <Route path="/detalleInstrumento">
-        <Route path=":idInstrumento" element={<DetalleInstrumento />} />
-      </Route>       */}
+        <Route path="/" element={<App />} />
         <Route path="/grillaManufacturado" element={<GrillaArticuloManufacturado />} />
         <Route path="/grillaInsumo" element={<GrillaArticuloInsumo />} />
-        {/* <Route path="/formulario/:idInstrumento" element={<Formulario/>} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
