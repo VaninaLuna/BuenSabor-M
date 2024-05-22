fd317b7db2861c6f47ad63520c6bf1968faeed89
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-05-22 18:42:17 -0300

    home y rutas

---
+++ Front/buenSaborFront/src/components/layout/Home.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+
+    return (
+        <>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h1>Bienvenido</h1>
+            </div>
+        </>
+    );
+}
--- Front/buenSaborFront/src/main.tsx
+++ Front/buenSaborFront/src/main.tsx
@@ -1,25 +1,44 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { GrillaArticuloManufacturado } from './components/GrillaArticuloManufacturado.tsx'
-import { GrillaArticuloInsumo } from './components/GrillaArticuloInsumo.tsx'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navegador } from './components/layout/Navegador.tsx'
 import React from 'react'
-import { GrillaCategoriaUnidadMedida } from './components/GrillaCategoriaUnidadMedida.tsx'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GrillaArticuloManufacturado } from './components/articulos/GrillaArticuloManufacturado.tsx'
+import { GrillaArticuloInsumo } from './components/articulos/GrillaArticuloInsumo.tsx'
+import { GrillaCategoriaUnidadMedida } from './components/articulos/GrillaCategoriaUnidadMedida.tsx'
+import Sidebar from './components/layout/Sidebar.tsx';
+import NavBar from './components/layout/Navbar.tsx';
+import { Home } from './components/layout/Home.tsx';
+import { GrillaEmpresa } from './components/empresa/GrillaEmpresa.tsx';
+import { GrillaSucursal } from './components/empresa/GrillaSucursal.tsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Navegador />
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/grillaManufacturado" element={<GrillaArticuloManufacturado />} />
-        <Route path="/grillaInsumo" element={<GrillaArticuloInsumo />} />
-        <Route path="/grillaCategoriaUnidadMedida" element={<GrillaCategoriaUnidadMedida />} />
-      </Routes>
+      <Sidebar />
+      <div className="h-100 w-100 flex-grow-1 ">
+        <NavBar />
+        {/* <Navegador /> */}
+        <div className='content' style={{ marginLeft: 60, marginRight: 60 }}>
+          <Routes>
+            <Route index element={<Home />} />
+            {/* EMPRESA */}
+            <Route path="/empresas" element={<GrillaEmpresa />} />
+            <Route path="/sucursales" element={<GrillaSucursal />} />
+
+            {/* ARTICULOS */}
+            <Route path="/insumos" element={<GrillaArticuloInsumo />} />
+            <Route path="/manufacturados" element={<GrillaArticuloManufacturado />} />
+            <Route path="/categorias" element={<GrillaCategoriaUnidadMedida />} />
+            <Route path="/unidadMedida" element={<GrillaCategoriaUnidadMedida />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   </React.StrictMode>,
 )
