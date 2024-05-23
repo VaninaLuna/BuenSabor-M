474ba2792d0d1a8c3e6014f7eaf63d6b0d5e1fa4
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-05-23 09:50:30 -0300

    base de datos

--- buenSabor.sql
+++ buenSabor.sql
@@ -31,11 +31,11 @@ CREATE TABLE `articulo` (
   KEY `FKlf2hbqm1r4qx36lkr0b4mix6b` (`unidad_medida_id`),
   CONSTRAINT `FK35xlp590328eybh2pf5ublsne` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`),
   CONSTRAINT `FKlf2hbqm1r4qx36lkr0b4mix6b` FOREIGN KEY (`unidad_medida_id`) REFERENCES `unidad_medida` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `articulo` */
 
-insert  into `articulo`(`id`,`denominacion`,`precio_venta`,`categoria_id`,`unidad_medida_id`) values (1,'Papas',22.54,2,1),(2,'Harina',22.5,1,2),(3,'Hamburguesa con papas',130,2,2),(4,'pizza',180,2,2),(9,'Asado',12,2,2),(13,'tomate',10,1,1),(15,'lechuga',5,1,1),(17,'ensalada',54,1,1),(18,'pollo crispy',100,2,2);
+insert  into `articulo`(`id`,`denominacion`,`precio_venta`,`categoria_id`,`unidad_medida_id`) values (1,'Papas',22.54,2,2),(2,'Harina',22.5,1,2),(3,'Hamburguesa con papas',130,3,1),(4,'pizza',180,6,2),(9,'Asado',12,7,1),(13,'Tomate',10,1,1),(15,'Lechuga',5,1,1),(17,'ensalada',54,5,1),(18,'pollo crispy',100,4,2),(20,'papas a la romana',41,2,2),(24,'Carne Molida',20,1,2),(25,'Carne en trozo',20,1,1);
 
 /*Table structure for table `articulo_insumo` */
 
@@ -53,7 +53,7 @@ CREATE TABLE `articulo_insumo` (
 
 /*Data for the table `articulo_insumo` */
 
-insert  into `articulo_insumo`(`es_para_elaborar`,`precio_compra`,`stock_actual`,`stock_maximo`,`id`) values (1,10.5,2,9,1),(1,10.5,2,10,2),(1,5,4,100,13),(1,3,12,50,15);
+insert  into `articulo_insumo`(`es_para_elaborar`,`precio_compra`,`stock_actual`,`stock_maximo`,`id`) values (1,10.5,2,9,1),(1,10.5,2,10,2),(1,5,4,100,13),(1,3,12,50,15),(1,10,1,2,24),(1,12,2,20,25);
 
 /*Table structure for table `articulo_manufacturado` */
 
@@ -70,7 +70,7 @@ CREATE TABLE `articulo_manufacturado` (
 
 /*Data for the table `articulo_manufacturado` */
 
-insert  into `articulo_manufacturado`(`descripcion`,`preparacion`,`tiempo_estimado_minutos`,`id`) values ('La mejor del pais','Poner el queso sobre la carne',10,3),('La mejor pizza del pais','pizza con anana',20,4),('postman con fritas','asado',123,9),('Contiene tomate y lechuga','Se cortan lo vegetales y se mezclan',15,17),('pollo frito','se frie el pollito',45,18);
+insert  into `articulo_manufacturado`(`descripcion`,`preparacion`,`tiempo_estimado_minutos`,`id`) values ('La mejor del pais','Poner el queso sobre la carne',10,3),('La mejor pizza del pais','pizza con anana',20,4),('postman con fritas','asado',123,9),('Contiene tomate y lechuga','Se cortan lo vegetales y se mezclan',15,17),('pollo frito','se frie el pollito',45,18),('papas con harina','se ponen las papas y se le mete harina',4,20);
 
 /*Table structure for table `articulo_manufacturado_detalle` */
 
@@ -86,11 +86,11 @@ CREATE TABLE `articulo_manufacturado_detalle` (
   KEY `FKcwnptwtrvdenu9tetlffab6dp` (`articulo_manufacturado_id`),
   CONSTRAINT `FKcwnptwtrvdenu9tetlffab6dp` FOREIGN KEY (`articulo_manufacturado_id`) REFERENCES `articulo_manufacturado` (`id`),
   CONSTRAINT `FKee8sad16ogk7in2nlh0vc3y9b` FOREIGN KEY (`articulo_insumo_id`) REFERENCES `articulo_insumo` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `articulo_manufacturado_detalle` */
 
-insert  into `articulo_manufacturado_detalle`(`id`,`cantidad`,`articulo_insumo_id`,`articulo_manufacturado_id`) values (1,15,1,3),(2,10,2,3),(3,5,2,4),(4,2,1,9),(12,5,13,3),(13,5,15,17),(14,1,13,17),(15,5,1,18),(16,5,1,17);
+insert  into `articulo_manufacturado_detalle`(`id`,`cantidad`,`articulo_insumo_id`,`articulo_manufacturado_id`) values (2,10,2,3),(3,5,2,4),(12,5,13,3),(13,5,15,17),(14,1,13,17),(15,5,1,18),(16,5,1,17),(27,10,1,20),(28,2,25,9),(30,1,2,20),(32,3,24,3),(33,1,15,3);
 
 /*Table structure for table `categoria` */
 
@@ -100,11 +100,44 @@ CREATE TABLE `categoria` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `denominacion` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `categoria` */
 
-insert  into `categoria`(`id`,`denominacion`) values (1,'Insumos'),(2,'Frituras');
+insert  into `categoria`(`id`,`denominacion`) values (1,'Insumos'),(2,'Frituras'),(3,'Hamburguesas'),(4,'Apanados'),(5,'Ensaladas'),(6,'Pizza'),(7,'Parrillada');
+
+/*Table structure for table `domicilio` */
+
+DROP TABLE IF EXISTS `domicilio`;
+
+CREATE TABLE `domicilio` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `calle` varchar(255) DEFAULT NULL,
+  `cp` int(11) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `localidad_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8t63gx4v022qapv45vdwld71j` (`localidad_id`),
+  CONSTRAINT `FK8t63gx4v022qapv45vdwld71j` FOREIGN KEY (`localidad_id`) REFERENCES `localidad` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `domicilio` */
+
+/*Table structure for table `empresa` */
+
+DROP TABLE IF EXISTS `empresa`;
+
+CREATE TABLE `empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cuil` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `razon_social` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `empresa` */
+
+insert  into `empresa`(`id`,`cuil`,`nombre`,`razon_social`) values (1,'23355175414','mal sabor','buensabito sa');
 
 /*Table structure for table `imagen` */
 
@@ -117,11 +150,74 @@ CREATE TABLE `imagen` (
   PRIMARY KEY (`id`),
   KEY `FKnlwl1vdnd0vsn7m8bhkvs85dh` (`articulo_id`),
   CONSTRAINT `FKnlwl1vdnd0vsn7m8bhkvs85dh` FOREIGN KEY (`articulo_id`) REFERENCES `articulo` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 /*Data for the table `imagen` */
 
-insert  into `imagen`(`id`,`url`,`articulo_id`) values (11,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',4),(22,'https://5aldia.cl/wp-content/uploads/2018/03/lechuga.jpg',15),(27,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREmpSVJB-rmNdW8Dfqqd9x4Pdq_6xO84Z7KeulIv1fkw&s',1),(28,'https://thefoodtech.com/wp-content/uploads/2020/06/Componentes-de-calidad-en-el-tomate-828x548.jpg',13),(29,'https://www.quironsalud.com/blogs/es/hablemos-nutricion/tomate.ficheros/2299323-tomate1.jpg?width=201&height=156&aspectRatio=true',13),(30,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',2),(32,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',3),(34,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',18),(36,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',9),(37,'https://www.quironsalud.com/blogs/es/hablemos-nutricion/tomate.ficheros/2299323-tomate1.jpg?width=201&height=156&aspectRatio=true',17),(38,'https://www.quironsalud.com/blogs/es/hablemos-nutricion/tomate.ficheros/2299323-tomate1.jpg?width=201&height=156&aspectRatio=true',17);
+insert  into `imagen`(`id`,`url`,`articulo_id`) values (30,'https://mandolina.co/wp-content/uploads/2023/03/648366622-1024x683.jpg',2),(58,'https://www.cronica.com.ar/__export/1701096141468/sites/cronica/img/2023/11/27/receta_de_pollo_crispy_crop1701095681090.png_974662632.png',18),(62,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP26O4HKVcIH3MlNnYc4I0afAKtU5nBD86zXZVNmfusg&s',4),(64,'https://www.quironsalud.com/blogs/es/hablemos-nutricion/tomate.ficheros/2299323-tomate1.jpg?width=201&height=156&aspectRatio=true',13),(65,'https://thefoodtech.com/wp-content/uploads/2020/06/Componentes-de-calidad-en-el-tomate-828x548.jpg',13),(66,'https://5aldia.cl/wp-content/uploads/2018/03/lechuga.jpg',15),(67,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREmpSVJB-rmNdW8Dfqqd9x4Pdq_6xO84Z7KeulIv1fkw&s',1),(69,'https://www.informador.mx/__export/1641927000980/sites/elinformador/img/2022/01/11/carne_molida_2_crop1641927000396.jpg_1902800913.jpg',24),(70,'https://s2.ppllstatics.com/diariovasco/www/multimedia/201906/03/media/cortadas/carne-roja-kS1C-R5uqUSGFlSqj84mn1I9bQuN-624x385@Diario%20Vasco.jpg',25),(71,'https://www.infocampo.com.ar/wp-content/uploads/2018/10/asado.jpg',9),(73,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKnx2ymt_E0ygi3ajuYGSYv-rLQ_uaYVA7LsOWHiZIQ&s',20),(77,'https://cdn.pixabay.com/photo/2022/08/29/17/45/burger-7419428_1280.jpg',3),(87,'https://www.comedera.com/wp-content/uploads/2021/12/ensalada-de-lechuga1.jpg',17);
+
+/*Table structure for table `localidad` */
+
+DROP TABLE IF EXISTS `localidad`;
+
+CREATE TABLE `localidad` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `provincia_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK37mbpxuicwnbo878s9djjgr39` (`provincia_id`),
+  CONSTRAINT `FK37mbpxuicwnbo878s9djjgr39` FOREIGN KEY (`provincia_id`) REFERENCES `provincia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `localidad` */
+
+/*Table structure for table `pais` */
+
+DROP TABLE IF EXISTS `pais`;
+
+CREATE TABLE `pais` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `pais` */
+
+/*Table structure for table `provincia` */
+
+DROP TABLE IF EXISTS `provincia`;
+
+CREATE TABLE `provincia` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `pais_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKm4s599988w0v1q1nw6dyo5t2m` (`pais_id`),
+  CONSTRAINT `FKm4s599988w0v1q1nw6dyo5t2m` FOREIGN KEY (`pais_id`) REFERENCES `pais` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `provincia` */
+
+/*Table structure for table `sucursal` */
+
+DROP TABLE IF EXISTS `sucursal`;
+
+CREATE TABLE `sucursal` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `casa_matriz` bit(1) DEFAULT NULL,
+  `horario_apertura` time(6) DEFAULT NULL,
+  `horario_cierre` time(6) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `domicilio_id` bigint(20) DEFAULT NULL,
+  `empresa_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_34hifwa9nn1cgdbjgkosx0wy2` (`domicilio_id`),
+  KEY `FK3w56rbjykxbp2e79cdq0xsghd` (`empresa_id`),
+  CONSTRAINT `FK3w56rbjykxbp2e79cdq0xsghd` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`),
+  CONSTRAINT `FKpxac8l3j9mwcwolj5dyctwpxo` FOREIGN KEY (`domicilio_id`) REFERENCES `domicilio` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `sucursal` */
 
 /*Table structure for table `unidad_medida` */
 
