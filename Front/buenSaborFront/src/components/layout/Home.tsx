3f26d51dc4ef3d009148c480a411e6e9e81ef210
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-06-13 17:28:17 -0300

    horario de atencion

--- Front/buenSaborFront/src/components/layout/Home.tsx
+++ Front/buenSaborFront/src/components/layout/Home.tsx
@@ -16,12 +16,15 @@ export function Home() {
     return (
         <>
             <div className="d-flex justify-content-center">
-                <h1 style={{ margin: '30px', alignSelf: "center" }}>Nuestras Promociones</h1>
+                <h1 style={{ margin: '30px', alignSelf: "center", color: 'whitesmoke' }}>Nuestras Promociones</h1>
             </div>
             <div className="d-flex flex-column w-100 justify-content-center m-auto">
                 <CCarousel controls transition="crossfade" indicators dark>
                     {carruselItems}
                 </CCarousel>
+                <h3 style={{ color: "whitesmoke", alignSelf: "center" }}>Horario de atención: </h3>
+                <p style={{ color: "whitesmoke", alignSelf: "center" }}><strong>Lunes a domingos de 20:00 a 12:00</strong></p>
+                <p style={{ color: "whitesmoke", alignSelf: "center" }}> <strong>Sábados y domingos de 11:00 a 15:00</strong></p>
             </div>
         </>
     );
