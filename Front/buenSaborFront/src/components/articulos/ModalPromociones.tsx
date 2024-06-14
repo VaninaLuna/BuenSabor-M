2d581cee7050a735a6840f8db0a39f1c9fccdece
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-06-14 12:39:05 -0300

    modal promocion

---
+++ Front/buenSaborFront/src/components/articulos/ModalPromociones.tsx
@@ -0,0 +1,88 @@
+import { Modal } from "react-bootstrap";
+import { Button, Form, Row, Col } from 'react-bootstrap';
+
+interface ModalProps {
+    showModal: boolean;
+    handleClose: () => void;
+    editing?: boolean;
+    selectedId?: number | null;
+}
+
+export const ModalPromociones: React.FC<ModalProps> = ({ showModal, handleClose }) => {
+    const handleCloseAndClear = () => {
+        handleClose();
+    };
+
+    return (
+        <Modal show={showModal} onHide={handleCloseAndClear} size="xl">
+            <Modal.Header closeButton>
+                <Modal.Title> Promocion</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Denominacion</Form.Label>
+                        <Form.Control type="text" name="denominacion" />
+                    </Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Fecha Desde</Form.Label>
+                                <Form.Control type="time" name="Fechadesde" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Fecha Hasta</Form.Label>
+                                <Form.Control type="time" name="Fechahasta" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Hora Desde</Form.Label>
+                                <Form.Control type="time" name="horaDesde" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Hora Hasta</Form.Label>
+                                <Form.Control type="time" name="horaHasta" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <Form.Group className="mb-3">
+                        <Form.Label>Denominacion del Descuento</Form.Label>
+                        <Form.Control type="text" name="denominacionDescuento" />
+                    </Form.Group>
+
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Precio</Form.Label>
+                                <Form.Control type="number" name="precio" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3">
+                                <Form.Label>Tipo de Promocion</Form.Label>
+                                <Form.Select aria-label="Default select example" name="tipoPromocion">
+                                    <option value={0}>Seleccionar tipo</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                </Form>
+            </Modal.Body>
+
+            <Modal.Footer className="d-flex justify-content-between">
+                <Button variant="secondary">Cancelar</Button>
+                <Button style={{ backgroundColor: '#83CA6A' }}>Guardar</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
--- Front/buenSaborFront/src/components/articulos/Promociones.tsx
+++ Front/buenSaborFront/src/components/articulos/Promociones.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import { Button, Table } from "react-bootstrap";
+import { ModalPromociones } from "./ModalPromociones";
 export function Promociones() {
 
+    const [showModal, setShowModal] = useState(false);
+    const [editing, setEditing] = useState(false);
+    const [selectedId, setSelectedId] = useState<number | null>(null);
 
+
+    const handleOpenCreate = () => {
+        setShowModal(true);
+        setEditing(false);
+        setSelectedId(null);
+    };
+    const handleClose = () => {
+        setShowModal(false);
+        setEditing(false);
+        setSelectedId(null);
+    };
 
     return (
         <>
             <div style={{ display: 'flex', justifyContent: 'top', flexDirection: 'column', alignItems: 'center', minHeight: '100vh' }}>
                 <h1 style={{ marginTop: '20px', color: "whitesmoke" }}>Promociones</h1>
-                <Button size="lg" style={{ margin: 20, backgroundColor: '#EE7F46', border: '#EE7F46' }}>
+                <ModalPromociones
+                    handleClose={handleClose}
+                    showModal={showModal}
+                    editing={editing}
+                    selectedId={selectedId}
+                />
+                <Button size="lg" style={{ margin: 20, backgroundColor: '#EE7F46', border: '#EE7F46' }} onClick={handleOpenCreate}>
                     Crear Promocion
                 </Button>
 
