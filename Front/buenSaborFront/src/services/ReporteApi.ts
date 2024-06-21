84130c8e9afe4b560b443badb4e456a46d79caf5
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-06-21 12:39:12 -0300

    reporteApi

---
+++ Front/buenSaborFront/src/services/ReporteApi.ts
@@ -0,0 +1,7 @@
+export async function getReporteFechas(fechaDesde: string, fechaHasta: string) {
+    const response = await fetch(`http://localhost:8080/report/excel?fechaDesde=${fechaDesde}&fechaHasta=${fechaHasta}`);
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(new Blob([blob]));
+
+    return url
+}
