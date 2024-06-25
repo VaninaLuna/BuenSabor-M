1fbd835a51c86054410482b4462c7cc341da7c2b
VaninaLuna <vanina.luna@alumnos.frm.utn.edu.ar>
2024-06-25 16:48:59 -0300

    home sin sidebar

--- Front/buenSaborFront/src/main.tsx
+++ Front/buenSaborFront/src/main.tsx
@@ -2,34 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { GrillaArticuloManufacturado } from './components/articulos/GrillaArticuloManufacturado.tsx'
-import { GrillaArticuloInsumo } from './components/articulos/GrillaArticuloInsumo.tsx'
-import Sidebar from './components/layout/Sidebar.tsx';
-import NavBar from './components/layout/Navbar.tsx';
-import { Home } from './components/layout/Home.tsx';
-// import { GrillaEmpresa } from './components/empresa/GrillaEmpresa.tsx';
-// import { GrillaSucursal } from './components/empresa/GrillaSucursal.tsx';
-import { Articulos } from './components/articulos/articulos.tsx';
-import RolUsuario from './components/controlAcceso/RolUsuario.tsx';
-import { RolName } from './models/RolName.ts';
-// import { RutaPrivada } from './components/controlAcceso/RutaPrivada.tsx';
-import { GrillaCategoria } from './components/articulos/GrillaCategoria.tsx';
-import { GrillaUnidadMedida } from './components/articulos/GrillaUnidadMedida.tsx';
-import { GrillaPedido } from './components/articulos/GrillaPedido.tsx';
-import { GrillaFactura } from './components/facturacion/GrillaFactura.tsx';
+import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react';
-import Profile from './components/auth/ProfileAuth0.tsx';
 import { AuthProvider } from './components/context/AuthContext.tsx';
-import { GrillaCliente } from './components/usuarios/GrillaCliente.tsx';
-import { GrillaEmpleado } from './components/usuarios/GrillaEmpleado.tsx';
-import { GrillaSuperUsuario } from './components/usuarios/GrillaSuperUsuario.tsx';
-import ReporteExcel from './components/reportes/ReporteExcel.tsx';
-import Estadisticas from './components/empresa/Estadisticas.tsx';
-import fondo from './assets/images/fondo3.jpg';
+import { App } from './App.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -43,79 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     >
       <AuthProvider>
         <BrowserRouter>
-          <Sidebar />
-          <div className="h-100 w-100 flex-grow-1" style={{
-            backgroundImage: `url(${fondo})`,
-            backgroundSize: 'auto',
-            backgroundPosition: 'center',
-            backgroundColor: '#e06f72',
-            // Este color será visible si la imagen no carga
-          }}>
-            <NavBar />
-            <div className='content' style={{ marginLeft: 60, marginRight: 60 }}>
-              <Routes>
-                {/* <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} /> */}
-                <Route path='/profile' element={<Profile />} />
-                <Route index element={<Home />} />
-                <Route path='/home' element={<Home />} />
-                <Route path="/articulos" element={<Articulos />} />
-                {/* EMPRESA */}
-                {/* <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/empresas" element={<GrillaEmpresa />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/sucursales" element={<GrillaSucursal />} />
-                </Route> */}
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/estadisticas" element={<Estadisticas />} />
-                </Route>
-
-                {/* ARTICULOS */}
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO, RolName.COCINERO]} />}>
-                  <Route path="/insumos" element={<GrillaArticuloInsumo />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO, RolName.COCINERO]} />}>
-                  <Route path="/manufacturados" element={<GrillaArticuloManufacturado />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO]} />}>
-                  <Route path="/categorias" element={<GrillaCategoria />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO]} />}>
-                  <Route path="/unidadMedida" element={<GrillaUnidadMedida />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO, RolName.COCINERO, RolName.CLIENTE]} />}>
-                  <Route path="/pedidos" element={<GrillaPedido />} />
-                </Route>
-
-                {/* FACTURACION */}
-                <Route element={<RolUsuario roles={[RolName.ADMIN, RolName.CAJERO, RolName.COCINERO, RolName.CLIENTE]} />}>
-                  <Route path="/facturacion" element={<GrillaFactura />} />
-                </Route>
-
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/reportes" element={<ReporteExcel />} />
-                </Route>
-
-                {/* USUARIOS */}
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/clientes" element={<GrillaCliente />} />
-                </Route>
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/empleados" element={<GrillaEmpleado />} />
-                </Route>
-                <Route element={<RolUsuario roles={[RolName.ADMIN]} />}>
-                  <Route path="/modificarRoles" element={<GrillaSuperUsuario />} />
-                </Route>
-              </Routes>
-            </div>
-          </div>
+          <App />
         </BrowserRouter>
       </AuthProvider>
     </Auth0Provider>
